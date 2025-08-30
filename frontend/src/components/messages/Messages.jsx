e647abb563bac1b45c88d52d02503f5cc2d4df8e
Shivamsiingh79 <shivamsiingh01@gmail.com>
2025-08-30 16:36:40 +0530

    application completed

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
+import React,{useEffect,useRef} from 'react'
 import Message from './Message'
+import userGetMessages from '../../hooks/userGetMessages'
+import MessageSkeleton from '../skeletons/MessageSkeleton';
+import useListenMessages from '../../hooks/useListenMessages';
+
 
 const Messages = () => {
+  const {messages,loading}=userGetMessages();
+
+  useListenMessages();
+  const lastMessageRef=useRef();
+
+
+  useEffect(() => {
+    setTimeout(()=> {
+      lastMessageRef.current?.scrollIntoView({behaviour:"smooth"});
+    },100);
+  }, [messages]);
+
+
   return (
     <div className='px-4 flex-1 overflow-auto'>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        
+      {!loading &&
+       messages.length>0 &&
+        messages.map((message)=>(
+          <div key={message._id}
+           ref={lastMessageRef}>
+            <Message  message={message}/>
+          </div>
 
+        ))}
+
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+      {!loading && messages.length === 0 && (
+        <p className='text-center'>Send a message to start the conversation</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Messages
 
---
+++ frontend/src/hooks/useGetConversation.js
@@ -0,0 +1,33 @@
+import React , { useState,useEffect} from 'react';
+import toast from 'react-hot-toast';
+
+const useGetConversation = () => {
+    const [loading,setLoading] = useState(false);
+    const [conversation,setConversations] = useState([]);
+
+    useEffect(()=>{
+        const getConversations = async() => {
+            setLoading(true);
+            try {
+                const res=await fetch('/api/users');
+                const data = await res.json();
+
+                if(data.error){
+                    throw new Error(data.error);
+                }
+
+                setConversations(data);
+            } catch (error) {
+                toast.error(error.message);
+            } finally{
+                setLoading(false);
+            }
+        }
+
+        getConversations();
+    },[]);
+
+    return {loading,conversation};
+}
+
+export default useGetConversation
